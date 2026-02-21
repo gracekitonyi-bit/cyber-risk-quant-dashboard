3cfa28e1d0f17f3192a40127f9aba0ab8eefbda8
Grace_Kitonyi. <grace.kitonyi@aims.ac.rw>
2026-02-21 20:19:09 +0200

    Create app.py

---
+++ app.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+st.set_page_config(page_title="Cyber Risk Quant Dashboard", layout="wide")
+
+st.title("🛡️ Cyber Risk Quantification Dashboard")
+st.write("Step 1 complete ✅ Repo setup is working.")
+
+st.info("Next: we will add Monte Carlo simulation for Annual Loss and breach probability.")
